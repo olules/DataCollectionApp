51a223dcff09afef1953aa1aefa5fc0cef3f8825
omarap <patrick.omara7@gmail.com>
2022-10-14 19:17:03 +0300

    second commit

--- api/models.py
+++ api/models.py
@@ -53,6 +53,9 @@ class Tree(models.Model):
     description = models.TextField(max_length=256, blank=True)
     price = models.PositiveIntegerField(null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
 class ProjectAffectedPerson(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
